745c664567653d23e805a409b4f7fa264052a68c
Rafael Ruiz <rafael@fuga.com>
2015-02-22 18:10:46 +0100

    Add Rspec config & Post validations

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do 
+	factory :post do |f|
+		f.title "Foobar"
+		f.body "Foo foo foobar foobar bar"
+	end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory"
+  it "is invalid without a title"
+  it "is invalid with a less than 5 chararacters title"
+  it "is invalid without a body"
 end
