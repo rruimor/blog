a73d3aab6c651769b3f2daa01770f3df5959f8ff
Rafael Ruiz <rafael@fuga.com>
2015-02-22 18:30:37 +0100

    Add edit & destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def create
-   @post = Post.new(post_params)
+    @post = Post.new(post_params)
 
     if @post.save
       redirect_to @post
@@ -19,6 +19,28 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order("created_at DESC")
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
