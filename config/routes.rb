eca53eff1bb78c57ba714c804ad759eabd620d33
Rafael Ruiz <rafael@fuga.com>
2015-02-22 23:13:00 +0100

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  get '/about', to: 'static_pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
